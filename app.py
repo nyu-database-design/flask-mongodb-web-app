dbc88060cf44ec057d6e30a9570a191f3892c9ae
bloombar <amos@cs.nyu.edu>
2021-03-30 22:20:42 -0400

    app.config

--- app.py
+++ app.py
@@ -14,18 +14,18 @@ app = Flask(__name__)
 # load credentials and configuration options from .env file
 # if you do not yet have a file named .env, make one based on the template in env.example
 import credentials
-config = credentials.get()
+app.config = credentials.get()
 
 # turn on debugging if in development mode
-if config['FLASK_ENV'] == 'development':
+if app.config['FLASK_ENV'] == 'development':
     # turn on debugging, if in development
     app.debug = True # debug mnode
 
 # make a single persistent database connection
-app.connection = pymongo.MongoClient(config['MONGO_HOST'], 27017, 
-                                username=config['MONGO_USER'],
-                                password=config['MONGO_PASSWORD'],
-                                authSource=config['MONGO_DBNAME'])
+app.connection = pymongo.MongoClient(app.config['MONGO_HOST'], 27017, 
+                                username=app.config['MONGO_USER'],
+                                password=app.config['MONGO_PASSWORD'],
+                                authSource=app.config['MONGO_DBNAME'])
 
 # set up the routes
 
@@ -43,7 +43,7 @@ def read():
     Route for GET requests to the read page.
     Displays some information for the user with links to other pages.
     """
-    collection = app.connection[config['MONGO_DBNAME']]["exampleapp"] # get the collection
+    collection = app.connection[app.config['MONGO_DBNAME']]["exampleapp"] # get the collection
     docs = collection.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
     return render_template('read.html', docs=docs) # render the read template
 
@@ -73,7 +73,7 @@ def create_post():
         "message": message, 
         "created_at": dt
     }
-    collection = app.connection[config['MONGO_DBNAME']]["exampleapp"] # get the collection
+    collection = app.connection[app.config['MONGO_DBNAME']]["exampleapp"] # get the collection
     collection.insert(doc_to_insert) # insert a new document
 
     return redirect(url_for('read')) # tell the browser to make a request for the /read route
@@ -85,7 +85,7 @@ def edit(mongoid):
     Route for GET requests to the edit page.
     Displays a form users can fill out to edit an existing record.
     """
-    collection = app.connection[config['MONGO_DBNAME']]["exampleapp"] # get the collection
+    collection = app.connection[app.config['MONGO_DBNAME']]["exampleapp"] # get the collection
     doc = collection.find_one({"_id": ObjectId(mongoid)})
     return render_template('edit.html', mongoid=mongoid, doc=doc) # render the edit template
 
@@ -102,7 +102,7 @@ def edit_post(mongoid):
     dt = datetime.datetime.now()
     dt_fmt = dt.strftime("%H:%M on %d %B %Y")
     doc_to_insert = {"_id": ObjectId(mongoid), "name": name, "message": message, "time": dt_fmt}
-    collection = app.connection[config['MONGO_DBNAME']]["exampleapp"] # get the collection
+    collection = app.connection[app.config['MONGO_DBNAME']]["exampleapp"] # get the collection
     collection.find_one_and_replace({"_id": ObjectId(mongoid)}, doc_to_insert)
 
     return redirect(url_for('read')) # tell the browser to make a request for the /read route
@@ -114,7 +114,7 @@ def delete(mongoid):
     Route for GET requests to the delete page.
     Deletes the specified record from the database, and then redirects the browser to the read page.
     """
-    collection = app.connection[config['MONGO_DBNAME']]["exampleapp"] # get the collection
+    collection = app.connection[app.config['MONGO_DBNAME']]["exampleapp"] # get the collection
     collection.find_one_and_delete({"_id": ObjectId(mongoid)})
     return redirect(url_for('read')) # tell the web browser to make a request for the /read route.
 
