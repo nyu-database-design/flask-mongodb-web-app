824b5b7acbd55ab261d5ec0a8cddf276a643384e
bloombar <amos@cs.nyu.edu>
2023-11-29 17:30:29 -0500

    remove options

--- app.py
+++ app.py
@@ -31,11 +31,11 @@ sentry_sdk.init(
     # enable_tracing=True,
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
+    # traces_sample_rate=1.0,
     # Set profiles_sample_rate to 1.0 to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
+    # profiles_sample_rate=1.0,
 )
 
 # instantiate the app
